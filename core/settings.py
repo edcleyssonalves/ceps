17d1302f7fcb05fefd6e17f7f4af695c0af09245
edcleyssonalves <edcleyssonalves@gmail.com>
2026-01-14 23:50:56 -0300

    adiciona o gunicorn, servidor wsgi para python

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app'] 
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
