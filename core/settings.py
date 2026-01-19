05720927ddd56ac8d1b366bd0b88093ffad96c4b
edcleyssonalves <edcleyssonalves@gmail.com>
2026-01-19 05:24:46 -0300

    ajusta CSRF_TRUSTED_ORIGINS para deploy

--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.railway.app",
+]
+
 ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
